839eac56772256d52d14c580c9c89c879db3cc15
Berkay Ülger <berkayulger45920@gmail.com>
2023-03-15 13:55:11 +0300

    DETAY SİZE COLOR

--- appMy/views.py
+++ appMy/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 def index(request):
     context = {}
@@ -13,9 +14,36 @@ def Contact(request):
     return render(request,'contact.html',context)
 
 def Shop(request):
-    context = {}
+    products = ProductStok.objects.all()
+    for i in products:
+        print(i.sizeletter.first())
+        for j in i.sizeletter.all():
+            print(j)
+    context = {
+        "products":products,
+    }
     return render(request,'shop.html',context)
 
-def ShopDetail(request):
-    context = {}
+def ShopDetail(request,slug):
+    product = get_object_or_404(ProductStok, product__slug = slug)
+    
+    listprice = []
+    listcolor = []
+    listsize = []
+    sizeprice = product.sizeletter.all()
+    for i in range(len(sizeprice)):
+        listprice.append(sizeprice[i].price)
+        listcolor.append(sizeprice[i].color.styletitle)
+        listsize.append(sizeprice[i].size.slug)
+    
+    print(listprice)
+    print(listcolor)
+    print(listsize)
+    print("============================")
+    if listcolor[0] == "black" and listsize[0] == "m":
+        print(listprice[0])
+    
+    context = {
+        "product":product,
+    }
     return render(request,'shop-single.html',context)
